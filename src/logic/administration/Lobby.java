42ee1763a6262da07b274ff304555f84a84807e7
Spacegod007 <spacegod007@outlook.com>
2017-11-27 15:12:49 +0100

    Javadoc - Lobby class
    Class Lobby:
    - Added javadoc comments

--- src/logic/administration/Lobby.java
+++ src/logic/administration/Lobby.java
@@ -9,37 +9,110 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Lobby implements Runnable, Serializable{
+public class Lobby implements Runnable, Serializable
+{
+    /**
+     * The id of the lobby
+     */
     private int id;
+
+    /**
+     * The host of the lobby
+     */
     private User host;
+
+    /**
+     * The name of the lobby
+     */
     private String name;
+
+    /**
+     * The players that are currently in the lobby
+     */
     private List<User> players;
+
+    /**
+     * The gamerules bound to this lobby by the host
+     */
     private List<Gamerule> gamerules;
+
+    /**
+     * The ipAddress used to connect to the server when the game starts
+     */
     private String ipAddress;
+
+    /**
+     * The port the server will be run on
+     */
     private final int port = 1111;
+
+    /**
+     * The maximum amount of players allowed
+     */
     private static int maxSize = 64; // static?
-    public User getHost() {
+
+    /**
+     * Gets the host of this lobby
+     * @return an User object containing the host of this lobby
+     */
+    public User getHost()
+    {
         return host;
     }
-    public int getId(){return id;}
 
-    public void setHost(User host) {
+    /**
+     * Gets the id of this lobby
+     * @return the id of this lobby
+     */
+    public int getId()
+    {
+        return id;
+    }
+
+    /**
+     * Sets the host of this lobby
+     * @param host to be set
+     */
+    public void setHost(User host)
+    {
         this.host = host;
     }
 
-    public ObservableList<User> getPlayers() {
+    /**
+     * Gets a list of all players in this lobby
+     * @return a list of users containing individual players
+     */
+    public ObservableList<User> getPlayers()
+    {
         return FXCollections.unmodifiableObservableList(FXCollections.observableList(players));
     }
 
-    public List<Gamerule> getGamerules() {
+    /**
+     * Gets a list of all the gamerules bound to this lobby
+     * @return a list of all gamerules bound to this lobby
+     */
+    public List<Gamerule> getGamerules()
+    {
         return Collections.unmodifiableList(gamerules);
     }
 
-    public String getName() {
+    /**
+     * Gets the name of this lobby
+     * @return
+     */
+    public String getName()
+    {
         return name;
     }
-    
-    public Lobby(String name, int id, String ipAddress){
+
+    /**
+     * Constructor of the lobby
+     * @param name of the lobby
+     * @param id of the lobby
+     * @param ipAddress of the host
+     */
+    public Lobby(String name, int id, String ipAddress)
+    {
         players = new ArrayList<>();
         gamerules = new ArrayList<>();
         this.name = name;
@@ -47,11 +120,17 @@ public class Lobby implements Runnable, Serializable{
         this.ipAddress = ipAddress;
     }
 
-    public boolean leave(int user){
+    /**
+     * Removes the specified userId from the lobby
+     * @param userId of the user who will be removed from the lobby
+     * @return true if the user was removed, false if the user was not removed
+     */
+    public boolean leave(int userId)
+    {
         try
         {
             for(User p : players){
-                if(p.getID() == user)
+                if(p.getID() == userId)
                 {
                     this.players.remove(p);
                     return true;
@@ -65,7 +144,13 @@ public class Lobby implements Runnable, Serializable{
         }
     }
 
-    public boolean join(User player){
+    /**
+     * Adds a player to the lobby
+     * @param player to be added to this lobby
+     * @return true if the player was added, false if the adding of the player failed
+     */
+    public boolean join(User player)
+    {
         try
         {
             if (host == null)
@@ -82,13 +167,28 @@ public class Lobby implements Runnable, Serializable{
             return false;
         }
     }
-    public void editGameRules(){
+
+    /**
+     * Changes the gamerules
+     */
+    public void editGameRules()
+    {
         throw new UnsupportedOperationException();
     }
-    public void startGame(){
+
+    /**
+     * Starts the game
+     */
+    public void startGame()
+    {
         throw new UnsupportedOperationException();
     }
-    public void migrateHost(){
+
+    /**
+     * Migrates the host if the host left the lobby
+     */
+    public void migrateHost()
+    {
         throw new UnsupportedOperationException();
     }
 
