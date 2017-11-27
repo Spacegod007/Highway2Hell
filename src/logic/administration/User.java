3a0f7538eb275a07f18e35a19549ca8b5d425c2f
Spacegod007 <spacegod007@outlook.com>
2017-11-27 15:22:44 +0100

    Javadoc - User class
    Class Lobby:
    - Added javadoc comments

--- src/logic/administration/User.java
+++ src/logic/administration/User.java
@@ -1,36 +1,77 @@
 package logic.administration;
 
-
 import java.io.Serializable;
 
+/**
+ * A user/player who plays the game
+ */
 public class User implements Serializable
 {
+    /**
+     * The username of the user
+     */
     private String username;
+
+    /**
+     * The id of the user
+     */
     private int ID;
+
+    /**
+     * The lobby the user is currently located in
+     */
     private Lobby activeLobby;
 
+    /**
+     * Gets the id of the user
+     * @return the id of the user
+     */
     public int getID()
     {
         return ID;
     }
+
+    /**
+     * Gets the active lobby of the user
+     * @return the lobby the user is currently located in
+     */
     public Lobby getActiveLobby()
     {
         return activeLobby;
     }
 
+    /**
+     * Sets the active lobby of the user
+     * @param activeLobby to be set
+     */
     public void setActiveLobby(Lobby activeLobby)
     {
         this.activeLobby = activeLobby;
     }
 
-    public String getUsername() {
+    /**
+     * Gets the username of the user
+     * @return the username of the user
+     */
+    public String getUsername()
+    {
         return username;
     }
 
-    public void setUsername(String username) {
+    /**
+     * Sets the username of the user
+     * @param username to be set
+     */
+    public void setUsername(String username)
+    {
         this.username = username;
     }
 
+    /**
+     * Constructs an user object
+     * @param username of the user
+     * @param ID of the user
+     */
     public User(String username, int ID) {
         this.username = username;
         this.ID = ID;
